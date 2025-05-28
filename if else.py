e8a713784b66c19327e4fa6bef434ba494136a27
kanagaraj3005 <kanagarajspark@gmail.com>
2025-05-28 19:00:54 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,24 @@
+username = input("Enter your username:")
+password = input("Enter your password:")
+if username == "raj" and password == "12345":
+    print("Login Sucessfully!")
+else:
+    print("Invalid credentials")
+
+
+mark = int(input("Enter your mark:")
+if mark > 100 or mark < 0:
+           print("Incorrect value")
+elif mark >= 90:
+    print("O Grade")
+elif mark >= 80:
+    print("A Grade")
+elif mark >= 70:
+    print("B Grade")
+elif mark >= 60:
+    print("C Grade")
+elif mark >= 50:
+    print("D Grade")
+else:
+    print("RA required")
+
---
+++ relational.py
@@ -0,0 +1,19 @@
+a = 50
+b = 100
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+
+num_1 = float(input("Enter your first num:")
+num_2 = float(input("Enter your second num:")
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
